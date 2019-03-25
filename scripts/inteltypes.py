4f2d90543013b0e11682503b06d7588adb7517d7
Deepak Nadig <deepnadig@gmail.com>
2019-03-25 16:55:36 -0500

    Updated MISP prepocessor. New logstash configuration.

--- scripts/inteltypes.py
+++ scripts/inteltypes.py
@@ -1,9 +1,37 @@
 type_dict = {
+    # IP Types
     "ip": "IP",
     "ip-src": "IP",
     "ip-dst": "IP",
     "src-ip": "IP",
     "dst-ip": "IP",
+    # Filehash types
     "md5": "FILEHASH",
-    "sha256": "FILEHASH"
+    "sha1": "FILEHASH",
+    "sha224": "FILEHASH",
+    "sha256": "FILEHASH",
+    "sha384": "FILEHASH",
+    "sha512": "FILEHASH",
+    "pehash": "FILEHASH",
+    # Domains, hostnames and URLs/URIs
+    "domain": "DOMAIN",
+    "hostname": "HOSTNAME",
+    "link": "LINK",
+    "url": "URL",
+    "uri": "URI",
+    # Others
+    "filename": "FILENAME",
+    "email-src": "EMAIL",
+    "text": "TEXT",
+    "comment": "INFO",
+    "yara": "YARA",
+    "regkey": "REGKEY",
+    "whois-registrant-email": "WHOIS",
+    "mutex": "MUTEX",
+    "btc": "BITCOIN",
+    # Data
+    "vulnerability": "PAYLOAD",
+    "malware-sample": "PAYLOAD",
+    "attachment": "PAYLOAD",
 }
+
--- scripts/json-feed-preprocessor.py
+++ scripts/json-feed-preprocessor.py
@@ -4,6 +4,7 @@ import os
 import sys
 from inteltypes import type_dict
 
+
 # Progress Bar
 def progress(count, total, status=''):
     bar_len = 60
@@ -69,46 +70,50 @@ def process_common_fields(data):
         print('Field "Attribute" not available. Skipping {}...'.format(filename))
     else:
         for item in data['Event']['Attribute']:
-            item['intel-analysis'] = intel_analysis
+            item['threat-analysis'] = intel_analysis
             item['threat-level-id'] = threat_level_id
-            item['intel-source'] = intel_info
-            item['intel-organization'] = intel_org
-            item['intel-published'] = intel_published
-            item['intel-timestamp'] = intel_timestamp
-            item['intel-category'] = item.pop('category')
+            item['threat-source'] = intel_info
+            item['threat-organization'] = intel_org
+            item['threat-published'] = intel_published
+            item['threat-timestamp'] = intel_timestamp
+            item['threat-category'] = item.pop('category')
             item['comments'] = item.pop('comment')
-            item['intel-uuid'] = item.pop('uuid')
+            item['threat-uuid'] = item.pop('uuid')
             item['exportable-to-ids'] = item.pop('to_ids')
-            item['intel-value'] = item.pop('value')
-            item['intel-type'] = item.pop('type')
 
-            # Additional preprocessing
-            if '|' in item['intel-type']:
-                var1 = item['intel-type']
-                item['intel-has-subtype'] = "true"
-                item['intel-subtype-1'] = var1.split('|')[1]
-                item['intel-type'] = var1.split('|')[0]
-            else:
-                item['intel-has-subtype'] = 'false'
-                item['intel-subtype-1'] =''
+            # Intel Types
+            item['threat-type'] = item.pop('type')
+            item['threat-type-metadata'] = ''
+            item['threat-has-types'] = 'false'
+            item['threat-type2'] = ''
+            item['threat-type2-metadata'] = ''
 
-            # Additional preprocessing
-            if '|' in item['intel-value']:
-                var2 = item['intel-value']
-                item['intel-has-values'] = "true"
-                item['intel-value-2'] = var2.split('|')[1]
-                item['intel-value'] = var2.split('|')[0]
-            else:
-                item['intel-has-values'] = 'false'
-                item['intel-value-2'] = ''
+            # Type(s) preprocessing
+            if '|' in item['threat-type']:
+                var1 = item['threat-type']
+                item['threat-has-types'] = 'true'
+                item['threat-type2'] = var1.split('|')[1]
+                item['threat-type'] = var1.split('|')[0]
 
-            if item['intel-type'] in type_dict.keys():
-                item['intel-subtype-1'] = item['intel-type']
-                item['intel-type'] = type_dict[item['intel-type']]
+            # Intel Values
+            item['threat-value'] = item.pop('value')
+            item['threat-has-values'] = 'false'
+            item['threat-value2'] = ''
 
-            # print(item['intel-has-subtype'] + "\t" + item['intel-type'] + "\t" + item['intel-subtype-1'])
-            # print(item['intel-has-values'] + "\t" + item['intel-value'] + "\t" + item['intel-value-2'])
-            # print(item['intel-type'])
+            # Value(s) preprocessing
+            if '|' in item['threat-value']:
+                var2 = item['threat-value']
+                item['threat-has-values'] = 'true'
+                item['threat-value2'] = var2.split('|')[1]
+                item['threat-value'] = var2.split('|')[0]
+
+            if item['threat-type'] in type_dict.keys():
+                item['threat-type2'] = item['threat-type']
+                item['threat-type'] = type_dict[item['threat-type']]
+
+            # print(item['threat-has-types'] + "\t" + item['threat-type'] + "\t" + item['threat-type2'])
+            # print(item['threat-has-values'] + "\t" + item['threat-value'] + "\t" + item['threat-value2'])
+            # print(item['threat-type'])
 
             local_event_list.append(item)
 
@@ -127,23 +132,34 @@ for filename in os.listdir(directory):
     data = json_file_reader(filename)
 
     events = list()
-    events, total_num_events = process_common_fields(data)
+    events, file_events = process_common_fields(data)
+    total_num_events += file_events
 
-    for event in events:
-        prefix = '{:d}'.format(i).zfill(8) + '-'
-        filename1 = prefix + filename
-        filename2 = os.fsencode(filename1)
+    # for event in events:
+    #     prefix = '{:d}'.format(i).zfill(8) + '-'
+    #     filename1 = prefix + filename
+    #     filename2 = os.fsencode(filename1)
+    #
+    #     local_event_data['Event'] = [events]
+    #
+    #     json_file_writer(local_event_data, filename2, target)
+    #
+    #     # Clear temporary local variables
+    #     local_event_data.clear()
+    #     i += 1
 
-        local_event_data['Event'] = [event]
+    filename2 = os.fsencode(filename)
 
-        json_file_writer(local_event_data, filename2, target)
+    local_event_data['Events'] = events
 
-        # Clear temporary local variables
-        local_event_data.clear()
-        i += 1
+    json_file_writer(local_event_data, filename2, target)
 
-    # Reset 'i'
-    i = 1
+    # Clear temporary local variables
+    local_event_data.clear()
+    # i += 1
+    #
+    # # Reset 'i'
+    # i = 1
 
 print('\nCompleted preprocessing successfully.\n')
 print('Total processed events: {:,}\n'.format(total_num_events))
