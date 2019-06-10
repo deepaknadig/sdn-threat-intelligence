5d8c35b667916160e211a28379532eb4a281e5a7
Deepak Nadig <deepnadig@gmail.com>
2019-06-10 00:08:39 -0500

    Added new configurations.

--- scripts/csv-downloader.sh
+++ scripts/csv-downloader.sh
@@ -9,6 +9,7 @@ INTEL_DIR=/home/$USER/intel/feeds/external
 
 wget -c "https://zeustracker.abuse.ch/blocklist.php?download=ipblocklist" -O $INTEL_DIR/001_zeustracker-abuse-ipblocklist.csv
 wget -c "https://zeustracker.abuse.ch/blocklist.php?download=compromised" -O $INTEL_DIR/002_zeustracker-abuse-compromised.csv
+
 wget -c "http://rules.emergingthreats.net/blockrules/compromised-ips.txt" -O $INTEL_DIR/003_emergingthreats-compromised.csv
 wget -c "https://panwdbl.appspot.com/lists/mdl.txt" -O $INTEL_DIR/004_appspot-malwaredomainlist.csv
 wget -c "http://cybercrime-tracker.net/all.php" -O $INTEL_DIR/005_cybercrime-tracker.csv
@@ -19,16 +20,28 @@ wget -c "https://raw.githubusercontent.com/pan-unit42/iocs/master/diamondfox/dia
 wget -c "https://home.nuug.no/~peter/pop3gropers.txt" -O $INTEL_DIR/010_pop3-gropers-ipblocklist.csv
 wget -c "https://ransomwaretracker.abuse.ch/feeds/csv/" -O $INTEL_DIR/011_ransomwaretracker-abuse.csv
 wget -c "https://feodotracker.abuse.ch/downloads/ipblocklist.csv" -O $INTEL_DIR/012_feodotracker-abuse-ipblocklist.csv
+
 wget -c "https://hosts-file.net/psh.txt" -O $INTEL_DIR/hphosts-file-psh-temp.csv
 wget -c "https://hosts-file.net/emd.txt" -O $INTEL_DIR/hphosts-file-emd-temp.csv
 wget -c "https://hosts-file.net/grm.txt" -O $INTEL_DIR/hphosts-domains-temp.csv
+
 wget -c "https://openphish.com/feed.txt" -O $INTEL_DIR/013_openphish-free.csv
 wget -c "https://raw.githubusercontent.com/ktsaou/blocklist-ipsets/master/firehol_level1.netset" -O $INTEL_DIR/014_firehol-level1.csv
+
 wget -c "http://osint.bambenekconsulting.com/feeds/c2-ipmasterlist-high.txt" -O $INTEL_DIR/015_bambek-c2-iplist.csv
-wget -c "http://osint.bambenekconsulting.com/feeds/c2-dommasterlist-high.txt" -O $INTEL_DIR/015_bambek-c2-domainlist.csv
+wget -c "http://osint.bambenekconsulting.com/feeds/c2-dommasterlist-high.txt" -O $INTEL_DIR/015_bambek-malicious-c2-domains.csv
 wget -c "http://osint.bambenekconsulting.com/feeds/dga-feed-high.csv" -O $INTEL_DIR/015_bambek-malicious-dga-domains.csv
-wget -c "http://cinsscore.com/list/ci-badguys.txt" -O $INTEL_DIR/cinsscore-ci-badguys.csv
-wget -c "http://reputation.alienvault.com/reputation.generic" -O $INTEL_DIR/alienvault-generic.csv
+
+wget -c "http://cinsscore.com/list/ci-badguys.txt" -O $INTEL_DIR/016_cinsscore-ci-badguys.csv
+wget -c "http://reputation.alienvault.com/reputation.generic" -O $INTEL_DIR/017_alienvault-generic.csv
+wget -c "https://lists.blocklist.de/lists/all.txt" -O $INTEL_DIR/018_blocklist-de-all.csv
+
+wget -c "https://dataplane.org/vncrfb.txt" -O $INTEL_DIR/019_dataplane-vnc.csv
+wget -c "https://dataplane.org/sshpwauth.txt" -O $INTEL_DIR/019_dataplane-ssh-attackers.csv
+wget -c "https://dataplane.org/sipregistration.txt" -O $INTEL_DIR/019_dataplane-sip-register.csv
+wget -c "https://dataplane.org/sipquery.txt" -O $INTEL_DIR/019_dataplane-sip-query.csv
+wget -c "https://dataplane.org/sipinvitation.txt" -O $INTEL_DIR/019_dataplane-sip-invitation.csv
+
 wget -c "https://sslbl.abuse.ch/blacklist/sslipblacklist.csv" -O $INTEL_DIR/sslbl-abuse-ipblacklist.csv
 wget -c "https://sslbl.abuse.ch/blacklist/dyre_sslipblacklist.csv" -O $INTEL_DIR/dyressl-abuse-ipblacklist.csv
 wget -c "https://blocklist.greensnow.co/greensnow.txt" -O $INTEL_DIR/greensnow-ipblocklist.csv
