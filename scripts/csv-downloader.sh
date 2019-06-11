5c2c7e519116255553f1dd6228dfca5536426b59
Deepak Nadig <deepnadig@gmail.com>
2019-06-11 08:52:53 -0500

    Added new configurations.

--- scripts/csv-downloader.sh
+++ scripts/csv-downloader.sh
@@ -12,7 +12,11 @@ wget -c "https://zeustracker.abuse.ch/blocklist.php?download=compromised" -O $IN
 
 wget -c "http://rules.emergingthreats.net/blockrules/compromised-ips.txt" -O $INTEL_DIR/003_emergingthreats-compromised.csv
 wget -c "https://panwdbl.appspot.com/lists/mdl.txt" -O $INTEL_DIR/004_appspot-malwaredomainlist.csv
+
 wget -c "http://cybercrime-tracker.net/all.php" -O $INTEL_DIR/005_cybercrime-tracker.csv
+wget -c "http://cybercrime-tracker.net/ccamlist.php" -O $INTEL_DIR/005_cybercrime-tracker-ccamlist.csv
+wget -c "http://cybercrime-tracker.net/ccamgate.php" -O $INTEL_DIR/005_cybercrime-tracker-ccamgate.csv
+
 wget -c "http://data.phishtank.com/data/online-valid.csv" -O $INTEL_DIR/006_phishtank.csv
 wget -c "http://dns-bh.sagadc.org/dynamic_dns.txt" -O $INTEL_DIR/007_listdynamic-dns-blocklist.csv
 wget -c "http://labs.snort.org/feeds/ip-filter.blf" -O $INTEL_DIR/008_snort-ip-filter.csv
@@ -21,9 +25,9 @@ wget -c "https://home.nuug.no/~peter/pop3gropers.txt" -O $INTEL_DIR/010_pop3-gro
 wget -c "https://ransomwaretracker.abuse.ch/feeds/csv/" -O $INTEL_DIR/011_ransomwaretracker-abuse.csv
 wget -c "https://feodotracker.abuse.ch/downloads/ipblocklist.csv" -O $INTEL_DIR/012_feodotracker-abuse-ipblocklist.csv
 
-wget -c "https://hosts-file.net/psh.txt" -O $INTEL_DIR/hphosts-file-psh-temp.csv
-wget -c "https://hosts-file.net/emd.txt" -O $INTEL_DIR/hphosts-file-emd-temp.csv
-wget -c "https://hosts-file.net/grm.txt" -O $INTEL_DIR/hphosts-domains-temp.csv
+wget -c "https://hosts-file.net/psh.txt" -O $INTEL_DIR/temp-hphosts-file-psh.csv
+wget -c "https://hosts-file.net/emd.txt" -O $INTEL_DIR/temp-hphosts-file-emd.csv
+wget -c "https://hosts-file.net/grm.txt" -O $INTEL_DIR/temp-hphosts-domains.csv
 
 wget -c "https://openphish.com/feed.txt" -O $INTEL_DIR/013_openphish-free.csv
 wget -c "https://raw.githubusercontent.com/ktsaou/blocklist-ipsets/master/firehol_level1.netset" -O $INTEL_DIR/014_firehol-level1.csv
@@ -42,36 +46,43 @@ wget -c "https://dataplane.org/sipregistration.txt" -O $INTEL_DIR/019_dataplane-
 wget -c "https://dataplane.org/sipquery.txt" -O $INTEL_DIR/019_dataplane-sip-query.csv
 wget -c "https://dataplane.org/sipinvitation.txt" -O $INTEL_DIR/019_dataplane-sip-invitation.csv
 
-wget -c "https://sslbl.abuse.ch/blacklist/sslipblacklist.csv" -O $INTEL_DIR/sslbl-abuse-ipblacklist.csv
-wget -c "https://sslbl.abuse.ch/blacklist/dyre_sslipblacklist.csv" -O $INTEL_DIR/dyressl-abuse-ipblacklist.csv
-wget -c "https://blocklist.greensnow.co/greensnow.txt" -O $INTEL_DIR/greensnow-ipblocklist.csv
-wget -c "http://www.cert.at/static/downloads/data/conficker/all_domains.txt" -O $INTEL_DIR/conficker-domains.csv
-wget -c "https://urlhaus.abuse.ch/downloads/csv/" -O $INTEL_DIR/urlhaus-abuse-malwaredomainlist.csv
-wget -c "http://api.cybercure.ai/feed/get_ips?type=csv" -O $INTEL_DIR/cybercure-ipblocklist-temp.csv
-wget -c "http://api.cybercure.ai/feed/get_url?type=csv" -O $INTEL_DIR/cybercure-blocked-urls-temp.csv
-wget -c "http://api.cybercure.ai/feed/get_hash?type=csv" -O $INTEL_DIR/cybercure-hashes-temp.csv
+wget -c "http://vxvault.net/URL_List.php" -O $INTEL_DIR/temp-vxvault.csv
+wget -c "https://sslbl.abuse.ch/blacklist/sslipblacklist.csv" -O $INTEL_DIR/021_sslbl-abuse-ipblacklist.csv
+wget -c "https://sslbl.abuse.ch/blacklist/sslblacklist.csv" -O $INTEL_DIR/022_sslbl-abuse-blacklist.csv
+wget -c "https://urlhaus.abuse.ch/downloads/csv/" -O $INTEL_DIR/023_urlhaus-abuse-malwaredomainlist.csv
+#wget -c "https://sslbl.abuse.ch/blacklist/dyre_sslipblacklist.csv" -O $INTEL_DIR/dyressl-abuse-ipblacklist.csv
+
+wget -c "https://blocklist.greensnow.co/greensnow.txt" -O $INTEL_DIR/024_greensnow-ipblocklist.csv
+wget -c "http://www.cert.at/static/downloads/data/conficker/all_domains.txt" -O $INTEL_DIR/025_conficker-domains.csv
+
+wget -c "https://gitlab.com/ZeroDot1/CoinBlockerLists/raw/master/list.txt?inline=false" -O $INTEL_DIR/026_coinblockers-domains-all.csv
+wget -c "https://gitlab.com/ZeroDot1/CoinBlockerLists/raw/master/list_optional.txt?inline=false" -O $INTEL_DIR/026_coinblockers-domains-optional.csv
+wget -c "https://gitlab.com/ZeroDot1/CoinBlockerLists/raw/master/list_browser.txt?inline=false" -O $INTEL_DIR/026_coinblockers-domains-browser.csv
+
+wget -c "http://api.cybercure.ai/feed/get_ips?type=csv" -O $INTEL_DIR/temp-cybercure-ipblocklist.csv
+wget -c "http://api.cybercure.ai/feed/get_url?type=csv" -O $INTEL_DIR/temp-cybercure-blocked-urls.csv
+wget -c "http://api.cybercure.ai/feed/get_hash?type=csv" -O $INTEL_DIR/temp-cybercure-hashes.csv
 wget -c "http://www.ipspamlist.com/public_feeds.csv" -O $INTEL_DIR/ipspamlist.csv
 #wget -c "https://www.dan.me.uk/torlist/?exit" -O $INTEL_DIR/tor-exit-nodes.csv
 #wget -c "https://www.dan.me.uk/torlist/" -O $INTEL_DIR/tor-all-nodes.csv
 
 # File Cleanups
-sed -i -e 's/\\"//g' $INTEL_DIR/011_ransomwaretracker-abuse.csv
-tr ',' '\n' < $INTEL_DIR/cybercure-ipblocklist-temp.csv > $INTEL_DIR/cybercure-ipblocklist.csv
-tr ',' '\n' < $INTEL_DIR/cybercure-blocked-urls-temp.csv > $INTEL_DIR/cybercure-blocked-urls.csv
-tr ',' '\n' < $INTEL_DIR/cybercure-hashes-temp.csv > $INTEL_DIR/cybercure-hashes.csv
 sed -i -e '$a\' $INTEL_DIR/004_appspot-malwaredomainlist.csv
+sed -i -e 's/\\"//g' $INTEL_DIR/011_ransomwaretracker-abuse.csv
+sed '1,7 {s/^/#/}' $INTEL_DIR/temp-hphosts-file-psh.csv > $INTEL_DIR/temp-hphosts-file-psh2.csv
+sed '1,7 {s/^/#/}' $INTEL_DIR/temp-hphosts-file-emd.csv > $INTEL_DIR/temp-hphosts-file-emd2.csv
+sed '1,7 {s/^/#/}' $INTEL_DIR/temp-hphosts-domains.csv > $INTEL_DIR/temp-hphosts-domains2.csv
+tr '\t' ',' < $INTEL_DIR/temp-hphosts-file-psh2.csv > $INTEL_DIR/hphosts-psh.csv
+tr '\t' ',' < $INTEL_DIR/temp-hphosts-file-emd2.csv > $INTEL_DIR/hphosts-emd.csv
+tr '\t' ',' < $INTEL_DIR/temp-hphosts-domains2.csv > $INTEL_DIR/hphosts-domains.csv
+sed '1,4 {s/^/#/}' $INTEL_DIR/temp-vxvault.csv > $INTEL_DIR/temp-vxvault2.csv
+tr -d '\r' < temp-vxvault2.csv > 020_vxvault-url-list.csv
+
+tr ',' '\n' < $INTEL_DIR/temp-cybercure-ipblocklist.csv > $INTEL_DIR/cybercure-ipblocklist.csv
+tr ',' '\n' < $INTEL_DIR/temp-cybercure-blocked-urls.csv > $INTEL_DIR/cybercure-blocked-urls.csv
+tr ',' '\n' < $INTEL_DIR/temp-cybercure-hashes.csv > $INTEL_DIR/cybercure-hashes.csv
 sed -i -e '$a\' $INTEL_DIR/cybercure-ipblocklist.csv
 sed -i -e '$a\' $INTEL_DIR/cybercure-blocked-urls.csv
 sed -i -e '$a\' $INTEL_DIR/cybercure-hashes.csv
 
-rm -rvf $INTEL_DIR/cybercure-ipblocklist-temp.csv $INTEL_DIR/cybercure-blocked-urls-temp.csv $INTEL_DIR/cybercure-hashes-temp.csv
-
-sed '1,7 {s/^/#/}' $INTEL_DIR/hphosts-file-psh-temp.csv > $INTEL_DIR/hphosts-file-psh-temp2.csv
-sed '1,7 {s/^/#/}' $INTEL_DIR/hphosts-file-emd-temp.csv > $INTEL_DIR/hphosts-file-emd-temp2.csv
-sed '1,7 {s/^/#/}' $INTEL_DIR/hphosts-domains-temp.csv > $INTEL_DIR/hphosts-domains-temp2.csv
-tr '\t' ',' < $INTEL_DIR/hphosts-file-psh-temp2.csv > $INTEL_DIR/hphosts-psh.csv
-tr '\t' ',' < $INTEL_DIR/hphosts-file-emd-temp2.csv > $INTEL_DIR/hphosts-emd.csv
-tr '\t' ',' < $INTEL_DIR/hphosts-domains-temp2.csv > $INTEL_DIR/hphosts-domains.csv
-
-
-rm -rvf $INTEL_DIR/hphosts-file-psh-temp* $INTEL_DIR/hphosts-file-emd-temp* $INTEL_DIR/hphosts-domains-temp*
+rm -rvf $INTEL_DIR/temp*
